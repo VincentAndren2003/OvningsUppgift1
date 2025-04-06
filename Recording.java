72d4eb46fb4d90697450d4d771ae5013b095fe61
Kasper Scott <kasperscott@gmail.com>
2025-04-06 15:32:49 +0200

    Added Recording.java

--- Recording.java
+++ Recording.java
@@ -1,2 +1,15 @@
+package OvningsUppgift1;
+
 public class Recording {
+
+    private String artist;
+    private String year;
+    private int condition;
+    private double price;
+
+    Recording(String name, String artist, int year, int condition, double price) {
+
+    }
+
+    public abstract String getType()
 }

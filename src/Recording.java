c04886a6c6d551fa2349054329719053691daca5
Pawnpug <popcorn.wennberg@gmail.com>
2025-04-06 16:37:53 +0200

    x

---
+++ src/Recording.java
@@ -0,0 +1,31 @@
+public abstract class Recording extends Item implements PriceableWithVAT25 {
+
+    private String artist;
+    private int year;
+    private int condition;
+    private double price;
+
+    public Recording (String name, String artist, int year, int condition, double price) {
+        super(name);
+        this.artist = artist;
+        this.year = year;
+        this.condition = condition;
+        this.price = price;
+    }
+
+    public String getType(){
+        return "Type";
+    }
+    public String getArtist() {
+        return artist;
+    }
+    public int getYear() {
+        return year;
+    }
+    public int getCondition() {
+        return condition;
+    }
+    public double getPrice() {
+        return price * (condition / 10d);
+    }
+}

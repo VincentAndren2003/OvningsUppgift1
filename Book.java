7fb373624c051c363692b2125d9cc3396df7f8b7
VincentAndren2003 <vian1167@student.su.se>
2025-04-06 15:26:24 +0200

    Ny Item Fil

---
+++ Book.java
@@ -0,0 +1,21 @@
+package OvningsUppgift1;
+///  Klassen Book
+/// Klassen Book¨ ar en konkret subklass till Item och representerar en bok som kan
+/// vara antingen inbunden eller inte (attributet bound). B¨ ocker ¨ ar belagda med
+/// 6% moms och ska d¨ arf¨ or implementera gr¨ anssnittet PriceableWithVAT6 (som
+/// ¨ arver fr˚an gr¨ anssnittet Priceable).
+/// Priset p˚a b¨ ocker p˚averkas av om boken ¨ ar inbunden eller inte. Inbundna b¨ ocker
+/// ¨
+/// ar v
+/// ¨ arda 30% mer.
+/// • getType som skall returnera str¨ angen ”Book”.
+/// ˚
+/// •
+/// Atkomstmetoder f¨ or alla sina attribut (price, bound, author).
+/// • getPrice som skall implementeras s˚a att den fungerar f¨ or att r¨ akna ut
+/// priset f¨ or en bok.
+/// Konstruktor:
+/// Book(String name, String author, double price, boolean bound)
+///
+public class Book {
+}

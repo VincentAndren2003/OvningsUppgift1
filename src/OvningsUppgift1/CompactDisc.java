79834507fa6883f771421d6ec7dfd594fe647dbc
Pawnpug <popcorn.wennberg@gmail.com>
2025-04-06 15:56:01 +0200

    x

--- src/OvningsUppgift1/CompactDisc.java
+++ src/OvningsUppgift1/CompactDisc.java
@@ -2,4 +2,11 @@ package OvningsUppgift1;
 
 public class CompactDisc extends Recording {
 
+    public CompactDisc(String name, String artist, int year, int condition, double price){
+
+    }
+
+    private String getType(){
+        return "CD";
+    }
 }

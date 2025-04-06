5077b63b5b906e032ca5793e93306f16fb29e50b
Kasper Scott <kasperscott@gmail.com>
2025-04-06 15:34:00 +0200

    Update Recording.java

--- Recording.java
+++ Recording.java
@@ -3,13 +3,15 @@ package OvningsUppgift1;
 public class Recording {
 
     private String artist;
-    private String year;
+    private int year;
     private int condition;
     private double price;
 
-    Recording(String name, String artist, int year, int condition, double price) {
+    public Recording (String name, String artist, int year, int condition, double price) {
 
     }
 
-    public abstract String getType()
+    public String getType(){
+        return "Recording";
+    }
 }

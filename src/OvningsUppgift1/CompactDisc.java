c702b9adc1c48579d7675d66d40114dd220b1c22
Kasper Scott <kasperscott@gmail.com>
2025-04-06 15:58:27 +0200

    Update Recording.java

--- src/OvningsUppgift1/CompactDisc.java
+++ src/OvningsUppgift1/CompactDisc.java
@@ -1,9 +1,9 @@
-package OvningsUppgift1;
+package src.OvningsUppgift1
 
 public class CompactDisc extends Recording {
 
     public CompactDisc(String name, String artist, int year, int condition, double price){
-
+        super(name, artist, year, condition, price);
     }
 
 

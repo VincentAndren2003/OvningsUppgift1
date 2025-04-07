c7d54b6fd134d3a8aa94c2c8105f3c3a1e937c2c
Pawnpug <popcorn.wennberg@gmail.com>
2025-04-07 13:41:12 +0200

    Merge remote-tracking branch 'origin/main'

--- src/Book.java
+++ src/Book.java
@@ -1,10 +1,10 @@
-public class Book extends Item implements PriceableWithVAT6{
+public class Book extends Item implements PriceableWithVAT6 {
 
     private final String author;
     private final double price;
     private final boolean bound;
 
-    public Book(String name, String author, double price, boolean bound){
+    public Book(String name, String author, double price, boolean bound) {
         super(name); // Ensures the required parameter 'name' is passed to the Item constructor
         this.author = author;
         this.price = price;
@@ -12,28 +12,34 @@ public class Book extends Item implements PriceableWithVAT6{
     }
 
 
-    private double price(){return price;}
+    private double price() {
+        return price;
+    }
 
-    private boolean bound(){
-        if(this.bound){
+    private boolean bound() {
+        if (this.bound) {
             return true;
         }
         return false;
     }
 
-    private String author(){return author;}
+    private String author() {
+        return author;
+    }
 
-    public double getPrice(){
-        if (this.bound()){
+    public double getPrice() {
+        if (this.bound()) {
             return price * 1.3;
         }
         return price;
     }
 
-    public String getType(){return "Book"; }
+    public String getType() {
+        return "Book";
+    }
 
     @Override
-    public String toString(){
+    public String toString() {
         return "name=" + getName() +
                 ", author =" + this.author +
                 ", bound=" + this.bound() +
